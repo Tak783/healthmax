7ec27d19e8177c57e526945efe5cd161682385ac
Tak Mazarura <tm.dev12@outlook.com>
2025-05-20 21:22:02 +0100

    Created HealthMax Workspace and iOS app

---
+++ Presentation/HealthMax/HealthMax/HealthMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthMaxApp.swift
+//  HealthMax
+//
+//  Created by Tak Mazarura on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Presentation/HealthMax/HealthMaxTests/HealthMaxTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthMaxTests.swift
+//  HealthMaxTests
+//
+//  Created by Tak Mazarura on 20/05/2025.
+//
+
+import Testing
+@testable import HealthMax
+
+struct HealthMaxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
