539a1b0e454e55f685aa666755b7b6d6cde8c600
Tak Mazarura <tm.dev12@outlook.com>
2025-06-02 17:31:41 +0100

    Added `HealthImprovement` model

---
+++ Core Healthmax/CoreHealthMaxModels/Sources/CoreHealthMaxModels/Improvement/HealthImprovementRecommendation.swift
@@ -0,0 +1,39 @@
+//
+//  HealthImprovement.swift
+//  CoreHealthMaxModels
+//
+//  Created on 02/06/2025.
+//
+
+import Foundation
+
+public struct HealthImprovementRecommendation: Codable {
+    public let metricType: HealthMetricType
+
+    public let currentValue: Double
+    public let targetValue: Double?
+    public let targetRange: ClosedRange<Double>?
+    public let targetTrend: TargetTrend
+
+    // Human-readable insight
+    public let summary: String
+    public let actions: [String]
+
+    public init(
+        metricType: HealthMetricType,
+        currentValue: Double,
+        targetValue: Double? = nil,
+        targetRange: ClosedRange<Double>? = nil,
+        targetTrend: TargetTrend,
+        summary: String,
+        actions: [String]
+    ) {
+        self.metricType = metricType
+        self.currentValue = currentValue
+        self.targetValue = targetValue
+        self.targetRange = targetRange
+        self.targetTrend = targetTrend
+        self.summary = summary
+        self.actions = actions
+    }
+}
---
+++ Core Healthmax/CoreHealthMaxModels/Sources/CoreHealthMaxModels/Improvement/TargetTrend.swift
@@ -0,0 +1,12 @@
+//
+//  TargetTrend.swift
+//  CoreHealthMaxModels
+//
+//  Created by Tak Mazarura on 02/06/2025.
+//
+
+public enum TargetTrend: String, Codable {
+    case increase
+    case decrease
+    case maintain
+}
