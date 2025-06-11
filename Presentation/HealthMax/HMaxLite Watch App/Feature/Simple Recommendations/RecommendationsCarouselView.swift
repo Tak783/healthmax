5e1ae47ca7f142ed54a84e157d693d0017bf19f5
Tak Mazarura <tm.dev12@outlook.com>
2025-06-11 20:48:59 +0100

    Apple watch app now works

--- Presentation/HealthMax/HMaxLite Watch App/Feature/Simple Recommendations/RecommendationsCarouselView.swift
+++ Presentation/HealthMax/HMaxLite Watch App/Feature/Simple Recommendations/RecommendationsCarouselView.swift
@@ -6,35 +6,52 @@
 //
 
 import SwiftUI
+import CorePresentation
 import UserBiometricsFeature
 
 struct RecommendationsCarouselView: View {
     @StateObject var viewModel: SimpleRecommendationsViewModel
 
     var body: some View {
-        Group {
-            if viewModel.isLoading {
-                ProgressView("Loading...")
-            } else if viewModel.recommendationPresentationModels.isEmpty {
-                Text("No recommendations yet!")
-                    .multilineTextAlignment(.center)
-            } else {
-                TabView {
-                    ForEach(viewModel.recommendationPresentationModels) { recomendation in
-                        VStack(spacing: 6) {
-                            Text(recomendation.emoji)
-                                .font(.largeTitle)
-                            Text(recomendation.title)
-                                .font(.headline)
-                            Text(recomendation.description)
-                                .font(.caption)
-                                .multilineTextAlignment(.center)
-                        }
-                        .padding()
+        ZStack {
+            DesignSystem.DSGradient.background
+                .ignoresSafeArea()
+
+            Group {
+                if viewModel.isLoading {
+                    ProgressView("Loading...")
+                } else if viewModel.recommendationPresentationModels.isEmpty {
+                    VStack(spacing: 4) {
+                        Text("👋")
+                            .font(.system(size: 20, weight: .bold))
+                            .multilineTextAlignment(.center)
+                        Text("No recommendations yet!")
+                            .font(.system(size: 13, weight: .bold))
+                            .multilineTextAlignment(.center)
+                        Text("Make sure you've set up Apple Health in HealthMax iPhone App")
+                            .font(.system(size: 12, weight: .light))
+                            .multilineTextAlignment(.center)
                     }
+                    .padding()
+                } else {
+                    TabView {
+                        ForEach(viewModel.recommendationPresentationModels) { recommendation in
+                            VStack(spacing: 6) {
+                                Text(recommendation.emoji)
+                                    .font(.title)
+                                Text(recommendation.title)
+                                    .font(.caption)
+                                Text(recommendation.description)
+                                    .font(.caption)
+                                    .multilineTextAlignment(.center)
+                            }
+                            .padding()
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
                 }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
             }
+            .padding()
         }
         .onAppear {
             Task {
