eec3c6faba856b8078de3a708094fa7bf4ee5941
Tak Mazarura <tm.dev12@outlook.com>
2025-06-11 13:40:59 +0100

    Added base Apple Watch ap

---
+++ Presentation/HealthMax/HMaxLite Watch App/App/HMaxLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMaxLiteApp.swift
+//  HMaxLite Watch App
+//
+//  Created by Tak Mazarura on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct HMaxLite_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Presentation/HealthMax/HMaxLite Watch App/Feature/Simple Recommendations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HMaxLite Watch App
+//
+//  Created by Tak Mazarura on 11/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
